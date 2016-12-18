d58575ad35478cbb0e0f03a44ba32d5b2e741d56
Hyhang <hyhang1996@gmail.com>
2016-12-18 15:14:32 +0800

    add game logic

--- support.js
+++ support.js
@@ -4,3 +4,38 @@ function getPosTop(i,j){
 function getPosLeft(i,j){
 	return 20+j*120;
 };
+
+function getNumberBackgroundColor(){
+	switch(number){
+		case 2 :return "#eee4da";
+		case 4 :return "#ede0c8";
+		case 8 :return "#f2b179";
+		case 16 :return "#f59563";
+		case 32 :return "#f67c5f";
+		case 64 :return "#f65e3b";
+		case 128 :return "#edcf72";
+		case 256 :return "#edcc61";
+		case 512 :return "#9c0";
+		case 1024 :return "#33b5e5";
+		case 2048 :return "#09c";
+		case 4096 :return "#a6c";
+		case 8192 :return "#93c";
+	}
+	return "black";
+}
+
+function getNumberColor(number){
+	if(number<=4){
+		return "#776e65";
+	}
+	return "white";
+}
+
+function nospace(){
+	for(var i = 0 ;i < 4; i++)
+		for(var j = 0; j <4; j++)
+			if(board[i][j] == 0){
+				return false;
+			}
+			return ture;
+}
